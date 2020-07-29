b7417489996119a77f2c4a983a9d5dc34bc6a959
Joseph Semrai <josephsemrai@gmail.com>
2020-07-29 16:41:29 -0400

    Initial profile updates

--- src/features/main/HomeScreen.tsx
+++ src/features/main/HomeScreen.tsx
@@ -1,9 +1,43 @@
 import React from 'react';
-import { Text, Button } from '@ui-kitten/components';
+import { Text, Button, Card } from '@ui-kitten/components';
 import DefaultLayout from '../../components/layouts/DefaultLayout';
 import { auth, db } from '../../components/FirebaseAuthenticator';
 import { RootState } from '../../app/rootReducer';
 import { useSelector } from 'react-redux';
+import firebase from 'firebase';
+import { View } from 'react-native';
+
+interface FirestorePassRepresenation {
+  id: string;
+  endTime: string;
+  fromLocation: string;
+  fromLocationName: string;
+  issuingUserUid: string;
+  passSchemaVersion: number;
+  startTime: string;
+  toLocation: string;
+  toLocationName: string;
+  type: string;
+  passRecipientName: string;
+  issuingUserName: string;
+}
+
+const HallPass = ({ passInformation }: { passInformation: FirestorePassRepresenation }) => {
+  const [setPassTimeLeft, passTimeLeft] = React.useState();
+
+  const Header = (props: any) => (
+    <View {...props}>
+      <Text category="h6">{passInformation.toLocationName}</Text>
+      <Text category="s1">{JSON.stringify(passInformation.endTime)}</Text>
+    </View>
+  );
+
+  return (
+    <Card status="success" header={Header}>
+      <Text>{passInformation.passRecipientName} </Text>
+    </Card>
+  );
+};
 
 const HomeScreen = () => {
   const userUid = useSelector((state: RootState) => state.setup.userUid);
@@ -18,21 +52,19 @@ const HomeScreen = () => {
       .doc(userUid)
       .onSnapshot(doc => {
         const profileData = doc.data();
-        unsubscribePasses = profileData.school
-          .get()
-          .then((doc: firebase.firestore.DocumentSnapshot) => {
-            doc.ref
-              .collection('passes')
-              .where('issuingUserUid', '==', userUid)
-              .onSnapshot((querySnapshot: firebase.firestore.QuerySnapshot) => {
-                setUserPasses(
-                  querySnapshot.docs.map(doc => {
-                    return { id: doc.id, ...doc.data() };
-                  })
-                );
-                console.log('Fetching more passes...');
-              });
-          });
+        profileData.school.get().then((doc: firebase.firestore.DocumentSnapshot) => {
+          unsubscribePasses = doc.ref
+            .collection('passes')
+            .where('issuingUserUid', '==', userUid)
+            .onSnapshot((querySnapshot: firebase.firestore.QuerySnapshot) => {
+              setUserPasses(
+                querySnapshot.docs.map(doc => {
+                  return { id: doc.id, ...doc.data() };
+                })
+              );
+              console.log('Fetching more passes...');
+            });
+        });
       });
 
     return () => {
@@ -43,12 +75,11 @@ const HomeScreen = () => {
 
   return (
     <DefaultLayout>
-      {userPasses &&
-        userPasses.map(pass => {
-          return <Text key={pass.id}>{pass.toLocationName}</Text>;
-        })}
-      <Text category="h1">{userUid}</Text>
       <Text category="h1">Your Passes</Text>
+      {userPasses &&
+        userPasses.map((pass: FirestorePassRepresenation) => {
+          return <HallPass key={pass.id} passInformation={pass} />;
+        })}
       <Button onPress={() => auth.signOut()}>Sign out</Button>
     </DefaultLayout>
   );
