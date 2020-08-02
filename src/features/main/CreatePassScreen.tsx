bc1117e987efbb30ce5b26d74e5725172ed72227
Joseph Semrai <josephsemrai@gmail.com>
2020-08-01 21:13:31 -0400

    Decouple scanner and student search

--- src/features/main/CreatePassScreen.tsx
+++ src/features/main/CreatePassScreen.tsx
@@ -50,7 +50,7 @@ export const StudentResultItem = ({
   );
 };
 
-const StudentSearch = ({ handleStudentSelect }: { handleStudentSelect: any }) => {
+const StudentSearch = ({ handleStudentSelect }: { handleStudentSelect: Function }) => {
   const schoolPath = useSelector((state: RootState) => state.setup.school.documentPath);
 
   const [searchText, setSearchText] = React.useState('');
@@ -72,34 +72,10 @@ const StudentSearch = ({ handleStudentSelect }: { handleStudentSelect: any }) =>
     { idField: 'uid' }
   );
 
-  // React.useEffect(() => {
-  //   console.log('Updated text', fallbackText);
-  //   db.doc(schoolPath)
-  //     .collection('students')
-  //     .orderBy('displayName')
-  //     .startAt(fallbackText)
-  //     .endAt(fallbackText + '\uf8ff')
-  //     .get()
-  //     .then(querySnapshot => {
-  //       let collectionData: any[] = [];
-  //       querySnapshot.forEach(doc => collectionData.push(doc.data()));
-  //       setMatchingUsers(collectionData);
-  //       console.log('Query searched: ' + fallbackText);
-  //       console.log(collectionData);
-  //     });
-  // }, [searchText]);
-
-  // if (isMatchingUsersCollectionLoading) {
-  //   return (
-  //     <>
-  //       <Spinner />
-  //     </>
-  //   );
-  // }
+  // TODO: Display any error
 
   return (
     <>
-      {/* <Text>{matchingUsersCollectionError?.message}</Text> */}
       <Input
         placeholder="Search for a student"
         value={searchText}
@@ -119,6 +95,75 @@ const StudentSearch = ({ handleStudentSelect }: { handleStudentSelect: any }) =>
     </>
   );
 };
+const Scanner = ({ handleStudentScan }: { handleStudentScan: Function }) => {
+  const [hasPermission, setHasPermission] = React.useState(null);
+  const [scanned, setScanned] = React.useState(false);
+
+  React.useEffect(() => {
+    // Potential source of error, no-op memory leak
+    BarCodeScanner.requestPermissionsAsync().then(({ status }) =>
+      setHasPermission(status === 'granted')
+    );
+  }, []);
+
+  if (hasPermission === null) {
+    return <Text>Requesting camera access to initiate scanning...</Text>;
+  }
+  if (hasPermission === false) {
+    return (
+      <Text>
+        No access to camera. Please open your settings and allow this app to access your camera.
+      </Text>
+    );
+  }
+
+  const handleBarCodeScanned = ({ type, data }: any) => {
+    setScanned(true);
+    handleStudentScan(data);
+  };
+
+  return (
+    <View
+      style={{
+        display: 'flex',
+        height: '100%',
+        width: '100%',
+      }}>
+      <Text category="h6" style={{ marginBottom: 10 }}>
+        Position the ID's barcode in frame
+      </Text>
+      <View style={{ flex: 3, borderRadius: 20 }}>
+        <Camera
+          style={[
+            StyleSheet.absoluteFillObject,
+            {
+              flex: 1,
+              width: '100%',
+              alignContent: 'center',
+              alignItems: 'center',
+              justifyContent: 'center',
+            },
+          ]}
+          barCodeScannerSettings={{
+            barCodeTypes: [BarCodeScanner.Constants.BarCodeType.code39],
+          }}
+          onBarCodeScanned={handleBarCodeScanned}>
+          <LottieView
+            loop
+            autoPlay
+            style={{
+              width: '100%',
+            }}
+            source={require('../../assets/barcodeScanning.json')}
+          />
+        </Camera>
+      </View>
+      <View style={{ flex: 1 }}></View>
+
+      {scanned && <Button onPress={() => setScanned(false)}>Scan Again?</Button>}
+    </View>
+  );
+};
 
 const CreatePassScreen = ({
   navigation,
@@ -127,121 +172,6 @@ const CreatePassScreen = ({
   navigation: CreatePassScreenNavigationProp;
   route: CreatePassScreenRouteProp;
 }) => {
-  const Scanner = () => {
-    // TODO: ONLY CAN BE USED INSIDE THE CREATEPASS SCREEN
-    const [hasPermission, setHasPermission] = React.useState(null);
-    const [scanned, setScanned] = React.useState(false);
-
-    React.useEffect(() => {
-      // Potential source of error, no-op memory leak
-      BarCodeScanner.requestPermissionsAsync().then(({ status }) =>
-        setHasPermission(status === 'granted')
-      );
-    }, []);
-
-    if (hasPermission === null) {
-      return <Text>Requesting camera access to initiate scanning...</Text>;
-    }
-    if (hasPermission === false) {
-      return (
-        <Text>
-          No access to camera. Please open your settings and allow this app to access your camera.
-        </Text>
-      );
-    }
-
-    const handleBarCodeScanned = ({ type, data }: any) => {
-      setScanned(true);
-      alert(`Bar code with type ${type} and data ${data} has been scanned!`);
-      navigation.navigate('StudentInfo', { schoolIssuedId: data, context: 'schoolIssuedId' });
-    };
-
-    return (
-      <View
-        style={{
-          display: 'flex',
-          height: '100%',
-          width: '100%',
-        }}>
-        <Text category="h6" style={{ marginBottom: 10 }}>
-          Position the ID's barcode in frame
-        </Text>
-        <View style={{ flex: 3, borderRadius: 20 }}>
-          {/* <BarCodeScanner
-            style={[
-              StyleSheet.absoluteFillObject,
-              {
-                flex: 1,
-                width: '100%',
-                alignContent: 'center',
-                alignItems: 'center',
-                justifyContent: 'center',
-              },
-            ]}
-            onBarCodeScanned={handleBarCodeScanned}>
-           
-          </BarCodeScanner> */}
-          <Camera
-            style={[
-              StyleSheet.absoluteFillObject,
-              {
-                flex: 1,
-                width: '100%',
-                alignContent: 'center',
-                alignItems: 'center',
-                justifyContent: 'center',
-              },
-            ]}
-            barCodeScannerSettings={{
-              barCodeTypes: [BarCodeScanner.Constants.BarCodeType.code39],
-            }}
-            onBarCodeScanned={handleBarCodeScanned}>
-            <LottieView
-              loop
-              autoPlay
-              style={{
-                width: '100%',
-              }}
-              source={require('../../assets/barcodeScanning.json')}
-            />
-          </Camera>
-        </View>
-        <View style={{ flex: 1 }}></View>
-
-        {scanned && <Button onPress={() => setScanned(false)}>Scan Again?</Button>}
-      </View>
-    );
-  };
-
-  const [user, isAuthLoading, authError] = useAuthState(auth);
-  // const [schoolData, setSchoolData] = React.useState<firebase.firestore.DocumentData>();
-  // const [userData, setUserData] = React.useState<firebase.firestore.DocumentData>();
-  // // const [userDoc, isUserDocLoading, userDocError] = useDocumentData(
-  // //   db.collection('users').doc(user.uid)
-  // // );
-  // React.useEffect(() => {
-  //   console.log('Fetching Firebase Data: User Profile Information && School Data');
-  //   const unsubscribe = db
-  //     .collection('users')
-  //     .doc(user.uid)
-  //     .onSnapshot(doc => {
-  //       const profileData = doc.data();
-  //       setUserData(profileData);
-  //       profileData.school
-  //         .get()
-  //         .then((doc: firebase.firestore.DocumentSnapshot) => setSchoolData(doc.data()));
-  //     });
-
-  //   return unsubscribe;
-  // }, []);
-
-  // if (!userData || !schoolData)
-  //   return (
-  //     <DefaultLayout>
-  //       <Spinner />
-  //     </DefaultLayout>
-  //   );
-
   if (Platform.OS === 'web' && route.params.context === 'scan') {
     alert(
       'Barcode scanning on the web version is not supported yet. Please use the manual search to add passes on the web'
@@ -263,15 +193,20 @@ const CreatePassScreen = ({
           }}
         />
 
-        <Text category="h1" style={{ marginBottom: 20 }}>
-          {route.params.context}
-        </Text>
-
         <Text category="h1" style={{ marginBottom: 5 }}>
           Create Pass
         </Text>
 
-        {route.params.context === 'scan' && <Scanner />}
+        {route.params.context === 'scan' && (
+          <Scanner
+            handleStudentScan={(data: any) =>
+              navigation.navigate('StudentInfo', {
+                schoolIssuedId: data,
+                context: 'schoolIssuedId',
+              })
+            }
+          />
+        )}
         {route.params.context === 'search' && (
           <StudentSearch
             handleStudentSelect={(student: Student) =>
@@ -279,11 +214,6 @@ const CreatePassScreen = ({
             }
           />
         )}
-
-        {/* <Text category="h2">{user.displayName}</Text>
-      <Text category="h3">{userData.role}</Text>
-      <Text category="p1">{schoolData.name}</Text>
-      <Button onPress={() => auth.signOut()}>Sign out</Button> */}
       </DefaultLayout>
     </>
   );
